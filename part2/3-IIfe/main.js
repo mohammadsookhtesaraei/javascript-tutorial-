1e87146026e96450f9079869e061e7c6143e5c6b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-12-13 10:40:33 +0330

    IIFE

---
+++ part2/3-IIfe/main.js
@@ -0,0 +1,12 @@
+
+
+// IIFE
+
+// immediately invoked function expression
+
+
+// فاکشن هایی که خودشون نتیجه رو اجرا میکنن
+
+(function(name,age){
+    console.log(`${name} is ${age}`)
+})("ali",25)
