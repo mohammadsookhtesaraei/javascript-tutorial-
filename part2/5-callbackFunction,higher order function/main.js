19738492d87d6950c35f58f3f414ea46d594e3fd
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-12-13 11:27:13 +0330

    callbackFunction

---
+++ part2/5-callbackFunction,higher order function/main.js
@@ -0,0 +1,17 @@
+
+
+// callback function
+
+
+const x=(text)=> console.log(`my name is ${text}`);
+
+
+
+function hell(name,callback){
+    const x=name;
+    callback(x);
+};
+
+
+hell("ali",x);
+
