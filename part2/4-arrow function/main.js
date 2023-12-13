116c4e195fcbb7d87a8a1a2d25400850eac57586
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2023-12-13 10:58:09 +0330

    arrow function

---
+++ part2/4-arrow function/main.js
@@ -0,0 +1,26 @@
+
+
+// arrow function
+
+const x=()=> 3*2;
+console.log(x());
+
+
+const m= a => a * 5;
+console.log(m(5));
+
+const y=(a,b) => a * b;
+console.log(y(2,5));
+
+
+const final=(a,b,c)=> {
+    const x=a *5;
+    const j=b*2;
+    const l=c *9;
+    return x + j + l;
+};
+
+
+console.log(final(3,3,3));
+
+
