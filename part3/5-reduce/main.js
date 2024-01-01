916962d50796fce85ff076182a4c820f9c668549
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2024-01-01 19:31:00 +0330

    reduce

---
+++ part3/5-reduce/main.js
@@ -0,0 +1,9 @@
+
+
+// reduce 
+
+const numbers=[20,12,17,18,19,20,14,15];
+
+const average=numbers.reduce((total,current)=> total + current ,0)/numbers.length;
+const final=average.toFixed(2);
+console.log(`Your average is ${final}`);
